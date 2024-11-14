3d4006d07f43a08b3eba61e74c32a9caa854847e
danyangc <98071715+danyangc@users.noreply.github.com>
2024-11-14 17:46:48 -0500

    added clean data script

---
+++ Code/00_clean_data.R
@@ -0,0 +1,15 @@
+here::i_am(
+  "Code/00_clean_data.R"
+)
+
+library(dplyr)
+library(lubridate)
+
+data_clean <- data %>%
+  filter(!is.na(Year), !is.na(Month), #remove rows with missing month and year
+         State != "United States") %>%  #remove the overall counts for the US
+  mutate(Date = make_date(Year, Month), #create new date column 
+         COVID.19.Deaths = as.numeric(COVID.19.Deaths)) %>% #make column numeric 
+  select(State, Place.of.Death, COVID.19.Deaths, Date)
+
+write.csv(data_clean, "Data/cleaned_data.csv", row.names = FALSE)
